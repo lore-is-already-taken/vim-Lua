422b69dc0f0d74ad7234d81ec29608de50b3e9d4
lore <lorenzosaavedra.c@gmail.com>
2023-07-05 20:33:41 -0400

    configs i did during day

---
+++ lua/after/ripgrep.lua
@@ -0,0 +1,1 @@
+require("ripgrep").setup({})
--- lua/main/set.lua
+++ lua/main/set.lua
@@ -19,7 +19,8 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
