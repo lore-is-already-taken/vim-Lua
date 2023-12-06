56b746da801b6e29a6cee6f95395d4dedf2007e3
lore <lorenosaavedra.c@gmail.com>
2023-12-06 16:44:23 +0100

    hola

--- lua/after/indent-blankline.lua
+++ lua/after/indent-blankline.lua
@@ -7,6 +7,7 @@ local highlight = {
 	"RainbowViolet",
 	"RainbowCyan",
 }
+
 local hooks = require("ibl.hooks")
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
--- lua/after/nvim-tree.lua
+++ lua/after/nvim-tree.lua
@@ -79,18 +79,18 @@ require("nvim-tree").setup({
 	view = {
 		side = "right",
 		adaptive_size = true,
-		float = {
-			enable = true,
-			quit_on_focus_loss = true,
-			open_win_config = {
-				relative = "editor",
-				border = "double",
-				width = 50,
-				height = 48,
-				row = 0,
-				col = 200,
-			},
-		},
+		--[[float = {]]
+		--[[enable = false,]]
+		--[[quit_on_focus_loss = true,]]
+		--[[open_win_config = {]]
+		--[[relative = "editor",]]
+		--[[border = "rounded",]]
+		--width = 50,
+		--height = 48,
+		--row = 0,
+		--col = 200,
+		--},
+		--},
 	},
 
 	actions = {
@@ -101,6 +101,69 @@ require("nvim-tree").setup({
 
 	renderer = {
 		group_empty = true,
+		indent_markers = {
+			enable = false,
+			inline_arrows = true,
+			icons = {
+				corner = "└",
+				edge = "│",
+				item = "│",
+				bottom = "─",
+				none = " ",
+			},
+		},
+		icons = {
+			web_devicons = {
+				file = {
+					enable = true,
+					color = true,
+				},
+				folder = {
+					enable = true,
+					color = true,
+				},
+			},
+			git_placement = "before",
+			modified_placement = "after",
+			diagnostics_placement = "signcolumn",
+			bookmarks_placement = "signcolumn",
+			padding = " ",
+			symlink_arrow = " ➛ ",
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+				modified = true,
+				diagnostics = true,
+				bookmarks = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				bookmark = "󰆤",
+				modified = "●",
+				folder = {
+					arrow_closed = "",
+					arrow_open = "",
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+					symlink_open = "",
+				},
+				git = {
+					unstaged = "✗",
+					staged = "✓",
+					unmerged = "",
+					renamed = "➜",
+					untracked = "★",
+					deleted = "",
+					ignored = "◌",
+				},
+			},
+		},
 	},
 
 	filters = {
