9301d31240afe99bc48e7b61bbd9e76c28417a54
lore <lorenzosaavedra.c@gmail.com>
2024-01-24 22:52:33 -0300

    hola

--- lua/after/conform.lua
+++ lua/after/conform.lua
@@ -7,20 +7,19 @@ conform.setup({
 		python = { "isort", "black" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = { { "prettierd", "prettier" } },
+		html = { { "prettier" } },
 	},
-	--[[
-	   [format_on_save = {
-	   [    lsp_fallback = true,
-	   [    async = false,
-	   [    timeout = 500,
-	   [},
-       ]]
+   --[[ format_on_save = {]]
+		--[[lsp_fallback = false,]]
+		--[[async = false,]]
+		--[[timeout = 10000,]]
+	--[[},]]
 })
 
 vim.keymap.set("n", "<leader>mp", function()
 	conform.format({
 		lsp_fallback = true,
-		async = false,
-		timeout = 500,
+		async = true,
+		timeout = 5000,
 	})
 end, { desc = "format format file on save" })
